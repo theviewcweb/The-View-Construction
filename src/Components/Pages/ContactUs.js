d11b49ada98f7515310cf6106fdbda8437b35169
Alex Gonzalez <alexmgonzalez99@gmail.com>
2022-03-20 18:57:38 -0500

    deploy website

--- src/Components/Pages/ContactUs.js
+++ src/Components/Pages/ContactUs.js
@@ -6,25 +6,27 @@ function ContactUs() {
     return (
         <>
             <div className="contact-us">
-                <div className="contact-content">
-                    <p>
-                        15824 Grant Rd,
-                        <br />
-                        Cypress, TX 77429
-                        <br />
-                        <br />
-                        Hours: Mon-Fri: 9:00AM - 5:00PM
-                    </p>
-                </div>
-                <div>
-                    <p className="contact-us-header">CONTACT US</p>
-                </div>
-                <div className="contact-content">
-                    <p>
-                        Phone: 281-541-6706
-                        <br />
-                        Email: theviewc@gmail.com
-                    </p>
+                <div className="contact-wrapper">
+                    <div className="contact-content">
+                        <p>
+                            15824 Grant Rd,
+                            <br />
+                            Cypress, TX 77429
+                            <br />
+                            <br />
+                            Hours: Mon-Fri: 9:00AM - 5:00PM
+                        </p>
+                    </div>
+                    <div>
+                        <p className="contact-us-header">CONTACT US</p>
+                    </div>
+                    <div className="contact-content">
+                        <p>
+                            Phone: 281-541-6706
+                            <br />
+                            Email: theviewc@gmail.com
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
